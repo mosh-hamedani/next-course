059fa6e7db980d850ec9877e1c134938c3d4fd57
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-09 11:36:20 -0700

    Routing and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
+    <main>
+      <h1>Hello World</h1>
+      <Link href="/users">Users</Link>
+    </main>
   )
 }
---
+++ app/users/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewUserPage = () => {
+  return (
+    <div>NewUserPage</div>
+  )
+}
+
+export default NewUserPage
