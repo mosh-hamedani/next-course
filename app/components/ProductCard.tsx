ac9e70845015ca7341297836f294730c34007605
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-11 09:30:53 -0700

    Tailwind

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import AddToCart from './AddToCart';
-import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
       <AddToCart />
     </div>
   )
