bf91d94d7d704be295447033ecd2ad6c8c2dae26
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-11 08:38:55 -0700

    Static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,12 +6,13 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
