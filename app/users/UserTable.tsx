c83bf0e8b9f2e9d3531f95f32713e2c50e783126
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-15 08:51:55 -0700

    Routing overview

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UserTable = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
+  const users: User[] = await res.json();
+
+  return (
+    <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td></tr>)}
+        </tbody>
+      </table>
+  )
+}
+
+export default UserTable
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,31 +1,11 @@
 import React from 'react'
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from './UserTable'
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>Users</h1>
-      <table className='table table-bordered'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td></tr>)}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   )
 }
