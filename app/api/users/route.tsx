07694986c43cd87a70ed5b3a9b3148db2d73d726
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-16 11:02:09 -0700

    Creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,10 @@ export function GET(request: NextRequest) {
     { id: 2, name: 'John' },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name) 
+    return NextResponse.json({ error: 'Name is required'}, { status: 400 })
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
