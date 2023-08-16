01a56f255ff69326a165249552895f50b9b55068
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-16 11:23:51 -0700

    Deleting an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -21,3 +21,13 @@ export async function PUT(
   
   return NextResponse.json({ id: 1, name: body.name })
 }
+
+export function DELETE(
+  request: NextRequest, 
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: 'User not found'}, { status: 404 });
+
+  return NextResponse.json({});
+}
