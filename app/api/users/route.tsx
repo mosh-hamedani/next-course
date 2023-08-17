0ac3f04daf2d6c5d6383f4aa0a9bf2b82eaadcdf
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-17 11:33:43 -0700

    Creating data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -11,7 +11,27 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
-  if (!validation.success) 
-    return NextResponse.json(validation.error.errors, { status: 400 })
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+    });
+
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json(
+      { error: "User already exists" },
+      { status: 400 }
+    );
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 const schema = z.object({
   name: z.string().min(3),
+  email: z.string().email()
 });
 
 export default schema;
