92197194789549eca101792935195ec59f573d81
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-17 11:59:04 -0700

    Exercise- Storing products

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import { prisma } from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: 'Milk', price: 2.5 },
-    { id: 2, name: 'Bread', price: 3.5 },
-  ])
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products)
 }
 
 export async function POST(request: NextRequest) {
@@ -13,5 +12,13 @@ export async function POST(request: NextRequest) {
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json({ id: 10, name: body.name, price: body.price }, { status: 201 })
+ 
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price
+    }
+  })
+  
+  return NextResponse.json(newProduct, { status: 201 })
 }
---
+++ prisma/migrations/20230817185153_create_product/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
