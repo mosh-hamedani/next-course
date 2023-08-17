01a739a9da3a3c2b5a5bb09db6955210d0d21368
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-17 10:54:58 -0700

    Creating migrations

---
+++ prisma/migrations/20230817173938_initial/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `followers` INTEGER NOT NULL DEFAULT 0,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
