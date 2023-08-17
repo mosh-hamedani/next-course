b2b2b36e4a83da81786cc0598d5e29a8fca22a04
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-17 11:40:54 -0700

    Updating data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -21,7 +21,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
@@ -30,13 +30,25 @@ export async function PUT(
       status: 400,
     });
 
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) }
+  });
+
+  if (!user)
     return NextResponse.json(
       { error: "User not found" },
       { status: 404 }
     );
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email
+    }
+  })
+
+  return NextResponse.json(updatedUser);
 }
 
 export function DELETE(
