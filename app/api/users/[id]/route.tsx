368733dc2744ce6ee3f7c8618b9d1122046ca742
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-17 11:26:05 -0700

    Getting data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,35 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "@/prisma/client";
 
-export function GET(
-  request: NextRequest, 
-  { params }: { params: { id: number } }) {
-    if (params.id > 10)
-      return NextResponse.json({ error: 'User not found'}, { status: 404 });
-    
-    return NextResponse.json({ id: 1, name: 'Mosh' });
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!user)
+    return NextResponse.json(
+      { error: "User not found" },
+      { status: 404 }
+    );
+
+  return NextResponse.json(user);
 }
 
 export async function PUT(
-  request: NextRequest, 
-  { params }: { params: { id: number } }) {
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 })
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+    });
 
   if (params.id > 10)
-    return NextResponse.json({ error: 'User not found'}, { status: 404 })
-  
-  return NextResponse.json({ id: 1, name: body.name })
+    return NextResponse.json(
+      { error: "User not found" },
+      { status: 404 }
+    );
+
+  return NextResponse.json({ id: 1, name: body.name });
 }
 
 export function DELETE(
-  request: NextRequest, 
+  request: NextRequest,
   { params }: { params: { id: number } }
 ) {
   if (params.id > 10)
-    return NextResponse.json({ error: 'User not found'}, { status: 404 });
+    return NextResponse.json(
+      { error: "User not found" },
+      { status: 404 }
+    );
 
   return NextResponse.json({});
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import { prisma } from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: 'Mosh' },
-    { id: 2, name: 'John' },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
