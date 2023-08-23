5141354d3b19f62641f06bd242d8104ae41126f9
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-23 10:56:59 -0700

    Signing out users

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -11,7 +11,11 @@ const NavBar = () => {
     <div className='flex bg-slate-200 p-3 space-x-3'>
       <Link href="/" className='mr-5'>Next.js</Link>
       <Link href="/users">Users</Link>
-      { status === 'authenticated' && <div>{session.user!.name}</div>}
+      { status === 'authenticated' && 
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className='ml-3'>Sign Out</Link>
+        </div>}
       { status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
     </div>
   )
