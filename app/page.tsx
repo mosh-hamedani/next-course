fe08767434b82fd554d97d3ea962587fb8874e5e
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-28 14:19:08 -0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
-import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
     <main>
-      <h1>Hello { session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
     </main>
   )
 }
