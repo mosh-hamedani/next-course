2d971e0647739c1e5c5f6170460b62348419d255
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-29 10:32:50 -0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-import Image from "next/image";
-import coffee from '@/public/images/coffee.jpg';
-
 export default async function Home() {
   return (
-    <main className="relative h-screen">
-      <Image 
-        src="https://bit.ly/react-cover" 
-        alt="Coffee"
-        fill
-        className="object-cover" 
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        quality={100}
-        priority
-        />
+    <main>
+      <h1>Hello World</h1>
     </main>
-  )
+  );
 }
