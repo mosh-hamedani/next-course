82bfe6f531816ef73d2747bfdfb7a371ba51554f
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-29 09:42:43 -0700

    Using third-party services

---
+++ app/GoogleAnalyticsScript.tsx
@@ -0,0 +1,19 @@
+import Script from 'next/script'
+import React from 'react'
+
+const GoogleAnalyticsScript = () => {
+  return (
+    <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-E720JHXSJ2" />
+    <Script>
+      {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-E720JHXSJ1');`}
+    </Script>
+    </>
+  )
+}
+
+export default GoogleAnalyticsScript
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import AuthProvider from './auth/Provider'
+import Script from 'next/script'
+import GoogleAnalyticsScript from './GoogleAnalyticsScript'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
+      <GoogleAnalyticsScript />
       <body className={inter.className}>
         <AuthProvider>
           <NavBar />
