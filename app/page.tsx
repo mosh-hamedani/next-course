9e50c60a05ad295fe11a880c04d581945f26d213
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-29 09:03:59 -0700

    Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import coffee from '@/public/images/coffee.jpg';
 
 export default async function Home() {
   return (
     <main>
+      <Image 
+        src="https://bit.ly/react-cover" 
+        alt="Coffee"
+        fill
+        className="object-cover" 
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={100}
+        priority
+        />
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bit.ly',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
