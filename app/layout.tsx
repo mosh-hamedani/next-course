9d333ff60babe01170bd7f992152ae3b80f821f3
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-29 11:08:47 -0700

    Using custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
+import localFont from 'next/font/local';
 import NavBar from './NavBar'
 import AuthProvider from './auth/Provider'
 import GoogleAnalyticsScript from './GoogleAnalyticsScript'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500']
+});
+
+const poppins = localFont({
+  src: '../public/fonts/poppins-regular-webfont.woff2',
+  variable: '--font-poppins'
+});
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <GoogleAnalyticsScript />
-      <body className={inter.className}>
+      <body className={poppins.variable}>
         <AuthProvider>
           <NavBar />
           <main className='p-5'>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: 'var(--font-poppins)'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
