569c1f7f65233321d04367c348b30327e3ae75d5
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-08-29 09:52:21 -0700

    Images

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import coffee from '@/public/images/coffee.jpg';
 
 export default async function Home() {
   return (
-    <main>
+    <main className="relative h-screen">
       <Image 
         src="https://bit.ly/react-cover" 
         alt="Coffee"
